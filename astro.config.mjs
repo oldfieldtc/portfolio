d43dff80a41d87b37f11ef2f9f25cd6e31528ec2
Tommy Oldfield <tco1@st-andrews.ac.uk>
2025-05-09 12:55:42 +0100

    FEAT Upgrade to Astro 5, analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tommyoldfield.co.uk",
-  output: "hybrid",
+  output: "static",
   adapter: cloudflare(),
   integrations: [mdx()],
   redirects: {
